5371d8e8b91e95c9d3bceb4e5d03c11b69022b91
mazyogz <yogiprass11@gmail.com>
2023-12-23 19:12:27 +0700

    7. Client Side Rendering

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,12 +1,10 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import ProductView from "../../views/Product";
+import useSWR from "swr";
+import {fetcher} from "../../lib/swr/fetcher"
 
-type productType = {
-  id: number;
-  name: string;
-  price: number;
-  size: string;
-};
+
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [products, setProducts] = useState([]);
@@ -17,20 +15,22 @@ const ProductPage = () => {
     }
   }, []);
 
-  useEffect(() => {
-    fetch("/api/product")
-      .then((res) => res.json())
-      .then((response) => {
-        setProducts(response.data);
-      });
-  });
+  const { data, error, isLoading } = useSWR(
+    "/api/product",
+    fetcher
+  );
+
+  // useEffect(() => {
+  //   fetch("/api/product")
+  //     .then((res) => res.json())
+  //     .then((response) => {
+  //       setProducts(response.data);
+  //     });
+  // });
   return (
-    <div>
-      <h1>Product Page</h1>
-      {products.map((product: productType) => (
-        <div key={product.id}>{product.name}</div>
-      ))}
-    </div>
+<div>
+  <ProductView products={isLoading ? [] : data.data}/>
+</div>
   );
 };
 
